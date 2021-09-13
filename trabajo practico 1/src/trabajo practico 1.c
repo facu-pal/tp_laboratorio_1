d45e3a4a67cd63061d388ca8aca16853f192847c
facu-pal <facundo.palmieri.01@gmail.com>
2021-09-13 12:12:12 -0300

    tp 1 facundo palmieri

---
+++ trabajo practico 1/src/trabajo practico 1.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : trabajo.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout, NULL);
+	printf("tp 1");
+
+	return EXIT_SUCCESS;
+}
